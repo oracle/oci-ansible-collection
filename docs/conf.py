bc7f8f46d3bd5d1ca01efb2f784af98fb47063e6
Ameya Lokre <ameya.lokre@oracle.com>
2020-08-31 14:48:12 -0700

    Update conf.py
    commenting out the source_parser to try out the build in readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -193,6 +193,6 @@ texinfo_documents = [
 # The suffix of source filenames.
 source_suffix = [".rst", ".md"]
 
-source_parsers = {
-    ".md": CommonMarkParser,
-}
+# source_parsers = {
+#     ".md": CommonMarkParser,
+# }
